17c36b1b0ff167a3ef886560fd9d48199a5a6921
Quang dat <Quangdat570@gmail.com>
2022-11-28 23:52:25 +0700

    done

--- src/page/comment/Comment.jsx
+++ src/page/comment/Comment.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createTodo,selectTodos } from "../../store/feautes/comments.slice";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const Comnent = () => {
     const dispatch = useDispatch();
     const { data: todos } = useSelector(selectTodos);
 
     return (
-        <div>
-            TodoList
-            
-            <div>
+        <div className="d-flex">
+            <div className="d-flex ">
+                <div>
+                    {todos.map((todo) => (
+                        <div key={todo.id}>{todo.name}</div>
+                    ))}
+                </div>
+                <div>
                 {todos.map((todo) => (
-                    <div key={todo.id}>{todo.name}</div>
-                ))}
+                        <div key={todo.id}>{todo.body}</div>
+                    ))}
+                </div>
             </div>
         </div>
     );
